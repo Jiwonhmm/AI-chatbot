6c75c77e2ac473cb4e998f43a94178dcb2cd3e4a
Gigi <gigi@JIWON-MacBook-Air.local>
2025-05-27 14:20:36 -0400

    clean initial commit (remove .env)

---
+++ chatbot.py
@@ -0,0 +1,15 @@
+from openai import OpenAI
+client = OpenAI()
+
+completion = client.chat.completions.create(
+  model="gpt-4.1",
+  messages=[
+    {"role": "developer", "content": "You are a helpful assistant."},
+    {"role": "user", "content": "Hello!"}
+  ]
+)
+
+while True: # for chat mechanism
+    message = input("User")
+
+print(completion.choices[0].message)
